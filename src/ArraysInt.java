f23290a16d6025541de8d84f8466d86f0f484b47
Paweł Goliszewski <pawel.goliszewski@comarch.com>
2021-10-11 17:09:04 +0200

    feat: second lecture

--- src/ArraysInt.java
+++ src/ArraysInt.java
@@ -1,13 +1,11 @@
-public class Arrays {
+public class ArraysInt {
     public static void main(String[] args) {
         //Declaration of an array
         int[] a;
-        int b[];
         //Initialization of an array
         a = new int[5];
-        b = new int[]{1, 2, 3}; // array literal
-        for (int i = 0; i < b.length; i++) {
-            System.out.println(b[i]);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
         }
     }
 }
--- src/Incrementation3.java
+++
@@ -1,8 +0,0 @@
-public class Incrementation3 {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = a++ + 5;
-        int c = --b - a++;
-        System.out.println(c);
-    }
-}
